c224b600907b42ca0238b51d495ce1bd2f412438
huangjinghe <18823298298@139.com>
2020-02-15 12:04:49 +0800

    V2.0(5G配套会审结果查询)

---
+++ application/index/controller/Nr.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\index\controller;
+
+
+class Nr extends Initial
+{
+    public function index(){
+        return $this->fetch();
+    }
+    public function ptgz(){
+        return $this->fetch();
+    }
+}
